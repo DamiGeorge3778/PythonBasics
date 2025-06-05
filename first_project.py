f60255d05c39ea8c1bd30192353e6613aad77c4b
Dami George <damio3778@gmail.com>
2025-06-05 11:21:00 -0500

    Age Calculator

--- first_project.py
+++ first_project.py
@@ -1,1 +1,8 @@
-print("12345")
+# create input
+print("I am going to try and guess your current age ")
+current_year = input("What year is it currently? ")
+birth_year = input("What year were you born? ")
+# calculate age
+age = int(current_year) - int(birth_year)
+birthday_age = age + 1
+print("You are either " + str(age) + " years old or if you have already have had your birthday then you are " + str(birthday_age) + " years old.")
