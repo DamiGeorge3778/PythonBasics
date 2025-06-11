6ff2eb3d74b5b4891f4399305b35cea30302d1ba
Dami George <damio3778@gmail.com>
2025-06-11 13:18:59 -0500

    Week 2

---
+++ Week_2/question_2.py
@@ -0,0 +1,31 @@
+# n = int(input())
+# for i in range(1, n):
+#     if i % 2 == 0:
+#         print(i)
+
+# While loops
+
+n = int(input())
+i = 1
+while i < n:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
+# 1st iteration 
+# n = 3, i = 1
+# 1 < 3 T
+# 1 % 2 == 1 F
+# i = 1 + 1 = 2
+
+# 2nd iteration
+# n = 3, i = 2
+# 2 < 3 T
+# 2 % 2 == 0 T
+# print(2)
+# i = 2 + 1 = 3
+
+# 3rd iteration
+# n = 3, i = 3
+# 3 < 3 F
+
+
---
+++ Week_2/question_3.py
@@ -0,0 +1,8 @@
+answer = 1
+n = int(input())
+for i in range(1, n + 1):
+    answer = answer * i
+print("Answer is", answer)
+# Print countdown from 10 to 1
+# Check if the number is a prime or not
+# Example: n = 7, print(7 is prime), n = 10, print(10 is not a prime)
