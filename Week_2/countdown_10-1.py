03cec5aff93c6dcccab4ad0821810390281100db
Dami George <damio3778@gmail.com>
2025-06-12 11:55:45 -0500

    Countdown from 10 to 1

---
+++ Week_2/countdown_10-1.py
@@ -0,0 +1,12 @@
+# Print countdown from 10 to 1
+# n = 10
+# while True:
+#     print(n)
+#     n -= 1
+#     if n == 0:
+#         break
+# For loop
+n = 10
+for i in range(n, 0, -1):
+    if (i % 2 != 0):
+        print(i)
