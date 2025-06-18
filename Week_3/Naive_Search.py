f65c61a72696ab9dc61055ffe599d9bc17502e66
DamiGeorge3778 <damio3778@gmail.com>
2025-06-18 12:52:37 -0500

    Naive Search

---
+++ Week_3/Naive_Search.py
@@ -0,0 +1,11 @@
+l =  [480, 296, 3993, 4668, 1483, 730, 6280, 1329, 4571, 625]
+question = int(input("Guess a number between 1 and 10,000 "))
+# if question in l:
+#     print("You guessed one of the numbers")
+# else:
+#     print("You did not guess one of the numbers")
+for i in range(len(l)):
+    if question == l[i]:
+        print("You guessed one of the numbers")
+        break
+    
