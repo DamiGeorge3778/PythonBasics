2d9c66baa5b3ecc8f4d8827b08c20926e79b4693
DamiGeorge3778 <damio3778@gmail.com>
2025-06-18 12:23:19 -0500

    Nested For-Loops

---
+++ Week_3/Nested_For-Loops.py
@@ -0,0 +1,8 @@
+shirts = ['a', 'b', 'c']
+pants = [1, 2, 3]
+hats = ['alpha', 'beta', 'gamma']
+
+for i in range(len(shirts)):
+    for j in range(len(pants)):
+        for k in range(len(hats)):
+            print(shirts[i], pants[j], hats[k])
