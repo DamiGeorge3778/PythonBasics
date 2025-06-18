0ef8ff863f5a1b6e35a0b0695e0952246450c4c6
DamiGeorge3778 <damio3778@gmail.com>
2025-06-18 12:55:41 -0500

    Made some changes

--- Week_3/Naive_Search.py
+++ Week_3/Naive_Search.py
@@ -4,8 +4,11 @@ question = int(input("Guess a number between 1 and 10,000 "))
 #     print("You guessed one of the numbers")
 # else:
 #     print("You did not guess one of the numbers")
+flag = 0
 for i in range(len(l)):
     if question == l[i]:
         print("You guessed one of the numbers")
+        flag = 1
         break
-    
+if flag == 0:
+    print("You guessed it wrong")
