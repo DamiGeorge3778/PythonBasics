5df183b9b65b31f43de5cc7aac50b7f302d2b982
DamiGeorge3778 <damio3778@gmail.com>
2025-06-19 11:34:08 -0500

    Added some spacing between each example

--- Week_3/Naive_Search.py
+++ Week_3/Naive_Search.py
@@ -1,9 +1,12 @@
 l =  [480, 296, 3993, 4668, 1483, 730, 6280, 1329, 4571, 625]
 g = int(input("Guess a number between 1 and 10,000 "))
+
 # if question in l:
 #     print("You guessed one of the numbers")
 # else:
 #     print("You did not guess one of the numbers")
+
+
 # flag = 0
 # for i in range(len(l)):
 #     if question == l[i]:
@@ -12,7 +15,11 @@ g = int(input("Guess a number between 1 and 10,000 "))
 #         break
 # if flag == 0:
 #     print("You guessed it wrong")
+
+
 # write a function search(list, input) this should return the output
+
+
 def guess_number(l, g):
     flag = 0
     for i in range(len(l)):
