17f728fef4a0aef1ceb4af3d03481969971b792e
DamiGeorge3778 <damio3778@gmail.com>
2025-06-27 12:46:25 -0500

    Made some changes

--- Week_3/Characters.py
+++ Week_3/Characters.py
@@ -1,14 +1,34 @@
 def count_uppercase(text):
-    return sum(1 for char in text if char.isupper())
-
+    # return sum(1 for char in text if char.isupper())
+    u_count = 0
+    for character in text:
+        if character.isupper():
+            u_count += 1
+    return u_count
 def count_lowercase(text):
-    return sum(1 for char in text if char.islower())
-
+    # return sum(1 for char in text if char.islower())
+    l_count = 0
+    for character in text:
+        if character.islower():
+            l_count += 1
+    return l_count
 def count_characters(text):
-    return len(text)
+    # return len(text)
+    character_count = 0
+    for character in text:
+        character_count += 1
+    return character_count
 
 def count_words(text):
-    return len(text.split())
+    # return len(text.split())
+    word_count = 0
+    for i in text:
+        if i == " ":
+            word_count += 1
+    return word_count + 1
+# Text = Good 
+# For i in text:
+#   print i
 
 def check_text(text):
     print("Text Analysis Results:")
