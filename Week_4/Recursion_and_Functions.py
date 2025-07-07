e87db0f7a2ae971a82c284dbb86aa5f0897ce9a8
DamiGeorge3778 <damio3778@gmail.com>
2025-07-07 12:19:27 -0500

    Have not started yet

---
+++ Week_4/Recursion_and_Functions.py
@@ -0,0 +1,10 @@
+# Question 3: Recursion and Functions
+# Write a recursive Python function to compute the sum of all elements in a nested list. The list can contain integers or other lists.
+
+# Example Input:
+
+# [1, [2, 3], [4, [5, 6]], 7]
+
+# Expected Output:
+
+# 28
